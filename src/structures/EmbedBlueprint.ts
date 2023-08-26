f45fd391dfcbcf6a3824e265948f46b93f521ccc
Dmitry <tina8069@yandex.ru>
2023-08-26 09:37:46 +0300

    added command queue

--- src/structures/EmbedBlueprint.ts
+++ src/structures/EmbedBlueprint.ts
@@ -1,18 +1,24 @@
-import {
-  ChannelType,
-  EmbedBuilder,
-  Message,
-  Snowflake,
-  TextBasedChannel,
-} from 'discord.js'
+import { EmbedBuilder } from 'discord.js'
 import DiscordMusicBot from './DiscordMusicBot'
 import { DiscordTrack } from './Dispatcher'
+import { msToISOString } from '../utils/ISO'
 
 export default class EmbedBlueprint {
   private _client: DiscordMusicBot
 
   constructor(client: DiscordMusicBot) {
     this._client = client
+  }
+
+  emptyQueue() {
+    const embed = new EmbedBuilder()
+      .setColor('Blue')
+      .setTitle('Очередь пуста')
+      .setDescription(
+        'Чтобы добавить треки в очередь, воспользуйтесь командой `/play`'
+      )
+
+    return embed
   }
 
   enqueuePlaylist(playlistInfo: {
@@ -55,6 +61,50 @@ export default class EmbedBlueprint {
       .setColor('Blue')
       .setThumbnail(track.info.thumbnailURL)
       .addFields([{ name: 'Сейчас играет', value }])
+
+    return embed
+  }
+
+  queue(
+    {
+      currentTrack,
+      position,
+    }: { currentTrack: DiscordTrack | null; position: number },
+    queue: DiscordTrack[]
+  ) {
+    const descriptionParts: string[] = []
+    const embed = new EmbedBuilder().setColor('Blue')
+    const title =
+      queue.length === 0 ? 'Очередь пуста' : `Треков в очереди: ${queue.length}`
+
+    if (currentTrack) {
+      const durationLeft = currentTrack.info.length - position
+      descriptionParts.push('**Сейчас играет**')
+      descriptionParts.push(
+        `[${currentTrack.info.title}](${
+          currentTrack.info.uri
+        }) \`${msToISOString(durationLeft)}\``
+      )
+    }
+
+    if (queue.length > 0) {
+      descriptionParts.push('', '**В очереди**')
+      queue.slice(0, 10).forEach((track, index) => {
+        const trackIndex = index + 1
+        descriptionParts.push(
+          `\`${trackIndex}\` [${track.info.title}](${
+            track.info.uri
+          }) \`${msToISOString(track.info.length)}\``
+        )
+      })
+    } else {
+      descriptionParts.push(
+        '',
+        `Чтобы добавить треки в очередь, воспользуйтесь командой \`/play\``
+      )
+    }
+
+    embed.setTitle(title).setDescription(descriptionParts.join('\n'))
 
     return embed
   }
