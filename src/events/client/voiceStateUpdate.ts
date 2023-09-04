2d05c26dbf75e00f3bc95530da631edb51cfef3c
Dmitry <tina8069@yandex.ru>
2023-09-04 19:11:48 +0300

    now bot will disconnect after a while if no people in channel

--- src/events/client/voiceStateUpdate.ts
+++ src/events/client/voiceStateUpdate.ts
@@ -1,18 +1,62 @@
-import { Events, VoiceState } from 'discord.js'
+import { Events, VoiceBasedChannel, VoiceState } from 'discord.js'
 import CEvent from '../../structures/CEvent'
+import DiscordMusicBot from '../../structures/DiscordMusicBot'
+
+const connectedToClientChannel = (
+  client: DiscordMusicBot,
+  newState: VoiceState
+) => {
+  const { channel } = newState
+  const clientId = client.user?.id
+  if (!channel) return false
+
+  return channel?.members.some(({ user }) => user.id === clientId)
+}
+
+const hasPeopleInChannel = (channel: VoiceBasedChannel) => {
+  return channel.members.some((member) => !member.user.bot)
+}
+
+const leavedFromClientChannel = (
+  client: DiscordMusicBot,
+  oldState: VoiceState
+) => {
+  const { channel } = oldState
+  const clientId = client.user?.id
+  if (!channel) return false
+
+  return channel?.members.some(({ user }) => user.id === clientId)
+}
 
 module.exports = new CEvent({
   name: Events.VoiceStateUpdate,
   executor: async (client, oldState: VoiceState, newState: VoiceState) => {
     const guildId = oldState.guild.id
     const dispatcher = client.subscription.get(guildId)
+    const isClient = oldState.member?.user.id === client.user?.id
+    const leaveDelay = client.config.leaveDelay
 
-    if (oldState.member?.user.id !== client.user?.id || !dispatcher) {
+    if (!dispatcher) {
       return
     }
 
-    if (newState.channel === null) {
-      dispatcher.destroy()
+    if (isClient) {
+      if (newState.channel === null) {
+        dispatcher.destroy()
+      }
+    } else {
+      if (connectedToClientChannel(client, newState)) {
+        dispatcher.timer.stop()
+      }
+      if (
+        oldState.channel &&
+        leavedFromClientChannel(client, oldState) &&
+        !hasPeopleInChannel(oldState.channel)
+      ) {
+        dispatcher.timer.set(leaveDelay, () => {
+          dispatcher.destroy()
+        })
+      }
     }
   },
 })
