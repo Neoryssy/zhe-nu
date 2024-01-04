3c66cedf3204778060d5ca7c76ca3032427dd134
Dmitry <tina8069@yandex.ru>
2024-01-04 21:15:31 +0300

    used socket listeners

--- apps/music-bot/src/server/sockets/index.ts
+++ apps/music-bot/src/server/sockets/index.ts
@@ -1,5 +1,8 @@
 import { Server as NetServer } from 'http'
 import { Server as ServerIO } from 'socket.io'
+import { playerListeners } from './listeners/player.listeners'
+import { voiceStateListeners } from './listeners/voiceState.listeners'
+import { queueListeners } from './listeners/queue.listeners'
 
 export const initializeServerIO = (httpServer: NetServer) => {
   const serverIO = new ServerIO(httpServer, {
@@ -8,6 +11,12 @@ export const initializeServerIO = (httpServer: NetServer) => {
     },
     path: '/api/socket/io',
     addTrailingSlash: false,
+  })
+
+  serverIO.on('connection', (socket) => {
+    queueListeners(socket)
+    voiceStateListeners(socket)
+    playerListeners(socket)
   })
 
   return serverIO
