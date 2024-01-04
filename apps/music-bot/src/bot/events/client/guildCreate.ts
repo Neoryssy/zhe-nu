2f1dc8bf68a7e531fb965f142515a72dc808aea7
Dmitry <tina8069@yandex.ru>
2024-01-04 14:33:13 +0300

    added guild model

--- apps/music-bot/src/bot/events/client/guildCreate.ts
+++ apps/music-bot/src/bot/events/client/guildCreate.ts
@@ -1,6 +1,7 @@
-import { Events, Guild } from 'discord.js'
+import { ChannelType, Events, Guild, TextChannel } from 'discord.js'
 import CEvent from '../../../structures/CEvent'
 import SlashCommandsManager from '../../../structures/SlashCommandsManager'
+import GuildModel from '../../../server/models/guild.model'
 
 module.exports = new CEvent({
   name: Events.GuildCreate,
@@ -11,6 +12,33 @@ module.exports = new CEvent({
       client.log.info(
         `Slash commands for guild ${guild.name} (${guild.id}) registered`
       )
+
+      const channels = guild.channels
+      let musicChannel = channels.cache.find((channel) =>
+        channel.type === ChannelType.GuildText && channel.name.includes('music')
+      ) as TextChannel | undefined
+
+      if (!musicChannel) {
+        musicChannel = await channels.create({
+          name: 'music',
+          type: ChannelType.GuildText,
+        })
+      }
+
+      let guildDocument = await GuildModel.findOne({ guildId: guild.id })
+
+      if (guildDocument) {
+        guildDocument.musicChannelId = musicChannel.id
+      } else {
+        guildDocument = new GuildModel({
+          guildId: guild.id,
+          musicChannelId: musicChannel.id,
+        })
+      }
+
+      await guildDocument.save()
+
+      musicChannel.send(`Текущий канал установлен для управления воспроизведением`)
     } catch (error) {
       client.log.error(error)
     }
