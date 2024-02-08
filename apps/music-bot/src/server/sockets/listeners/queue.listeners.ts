8fa140ea0f4225e0fef853b18e687b85f1476db3
Dmitry <tina8069@yandex.ru>
2024-02-08 20:00:13 +0300

    added moveTrack socket listener

--- apps/music-bot/src/server/sockets/listeners/queue.listeners.ts
+++ apps/music-bot/src/server/sockets/listeners/queue.listeners.ts
@@ -1,8 +1,20 @@
 import { Socket } from 'socket.io'
 import { QueueSocketEmitter } from '../emitters/queue.emitter'
+import discordClient from '../../../bot/discordClient'
 
 export const queueListeners = (socket: Socket) => {
   socket.on('queue:get', (guildId: string) => {
     QueueSocketEmitter.emitQueue(guildId)
   })
+
+  socket.on('queue:moveTrack', (guildId: string, from: number, to: number) => {
+    const dispatcher = discordClient.subscription.get(guildId)
+
+    if (!dispatcher) {
+      return
+    }
+
+    dispatcher.moveTrack(from, to)
+    QueueSocketEmitter.emitQueue(guildId)
+  })  
 }
